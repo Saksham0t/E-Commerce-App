dbdf7d4487b82c7be611603c219c3b61252e984b
Gupta <saksham03gupta@gmail.com>
2025-09-15 18:16:00 +0530

    initial commit

---
+++ src/app/footer/footer.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css'
+})
+export class Footer {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
