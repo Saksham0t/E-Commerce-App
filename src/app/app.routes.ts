1f9f4e744f1a7cc9d173a904768e0ec6d9a68ddb
Gupta <saksham03gupta@gmail.com>
2025-09-16 18:29:39 +0530

    18:29

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Cart } from './Shopping_Cart/cart/cart';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'cart', component: Cart},
+
+];
