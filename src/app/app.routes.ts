19c07f6cb16869f13aace0ac2ad913c0cf77d2aa
Patil <2440525@cognizant.com>
2025-09-17 16:50:29 +0530

    merging

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+// //import { Checkout } from './Order_Management/checkout/checkout';
+// //import { OrderConfirmation } from './Order_Management/order-confirmation/order-confirmation';
 
-export const routes: Routes = [];
+// export const routes: Routes = [
+//     {path:'checkout',component:Checkout},
+//     {path:'orders/confirmation/:id',component:OrderConfirmation},
+//     //{path:'',redirectTo:'/orders',pathMatch:'full'}
+// ];
