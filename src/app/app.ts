77f914a96abb1be597637829c756cb7c0b07647c
Patil <2440525@cognizant.com>
2025-09-19 18:42:20 +0530

    lomesh

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, NgModule, signal } from '@angular/core';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { OrderList } from './Order_Management/order-list/order-list';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer,RouterModule],
+  imports: [Header, Footer, RouterModule, OrderList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
 
+  // @NgModule({
+  //   declarations:[
+  //     OrderList
+  //   ]
+  // })
 }
