75be0133a22e6c6c47eff8495b5915a1dfee1057
shruti <2441297@cognizant.com>
2025-09-22 18:41:15 +0530

    shruti

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import {  RouterModule, RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'; //s
+//import { ProductList } from './Product_Management/product-list/product-list';//s
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer,RouterModule],
+  imports: [Header, Footer,RouterModule,HttpClientModule,RouterOutlet],   //s-3
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
